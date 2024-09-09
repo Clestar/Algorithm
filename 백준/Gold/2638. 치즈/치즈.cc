657130698fc776413bfd7e1b400848b968668dfc
Clestar <44086091+Clestar@users.noreply.github.com>
2024-09-09 14:06:44 +0900

    [Gold III] Title: 치즈, Time: 20 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Gold/2638. 치즈/치즈.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int arr[100][100];
+bool visited[100][100];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int main() {
+	int N, M; cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			int tmp; cin >> tmp;
+			arr[i][j] = tmp;
+		}
+	}
+	int day = 0;
+	while (true) {
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				visited[i][j] = false;
+			}
+		}
+		queue<pair<int, int>>q;
+		q.push({ 0,0 });
+		visited[0][0] = true;
+		while (!q.empty()) {
+			int x = q.front().first;
+			int y = q.front().second;
+			q.pop();
+			cnt++;
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx < 0 || nx >= M || ny < 0 || ny >= N)continue;
+				if (arr[ny][nx] == 1)continue;
+				if (visited[ny][nx])continue;
+				q.push({ nx,ny });
+				visited[ny][nx] = true;
+			}
+		}
+		if (cnt == N * M)break;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				int cnt = 0;
+				for (int k = 0; k < 4; k++) {
+					int nx = j + dx[k];
+					int ny = i + dy[k];
+					if (nx < 0 || nx >= M || ny < 0 || ny >= N)continue;
+					if (arr[ny][nx] == 1)continue;
+					if (visited[ny][nx]==false)continue;
+					cnt++;
+				}
+				if (cnt >= 2)arr[i][j] = 0;
+			}
+		}
+		day++;
+	}
+	cout << day << endl;
+}
